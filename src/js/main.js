0370e183688ab1d6724c01488df6d08b9b4455d3
NataliaIvasiv <143248403+NataliaIvasiv@users.noreply.github.com>
2024-02-08 14:58:49 +0200

    done

--- src/js/main.js
+++ src/js/main.js
@@ -5,30 +5,48 @@ import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('form');
-const searchBtn = document.querySelector('button');
+
 const imagesList = document.querySelector(".gallery")
+const loader = document.querySelector(".loader")
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+    imagesList.innerHTML = '';
     const keyWord = event.target.elements.search.value;
-    
+    loader.classList.remove('is-hidden')
     fetchImages(keyWord)
-        .then((images) => renderImages(images))
-    .catch((error)=>console.log(error))
+        .then((images) => {
+            validation(images);
+            renderImages(images);
+            
+            loader.classList.add('is-hidden');
+        })
+        .catch((error) => console.log(error))
     
+    event.target.reset();
 })
 
-
+function validation(images) {
+    if (!images.total) {
+        iziToast.error({
+    title: '',
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+    position: 'topRight',
+});
+    }
+}
 
 function fetchImages(keyWord) {
     return fetch(`https://pixabay.com/api/?key=42236651-09dd8ef8cae726de85d6e38a7&q=${keyWord}&image_type=photo&orientation=horizontal&safesearch=true`).then((response) => {
+ 
         if (!response.ok) {
-            
             throw new Error(response.status);
         }
         return response.json();
     });
 }
+
+
 
 function renderImages(images){
     const markup = images.hits
@@ -45,10 +63,15 @@ function renderImages(images){
   </a>`;
         })
         .join("");
-    imagesList.insertAdjacentHTML("beforeend", markup);
+    imagesList.innerHTML = markup;
     let gallery = new SimpleLightbox('.gallery a', options);
-gallery.on('show.simplelightbox');
+    gallery.on('show.simplelightbox');
+    gallery.refresh();
 }
+
+
+
+
 const options = {
   captions: true,
   captionSelector: 'img',
